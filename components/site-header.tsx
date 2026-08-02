d113908019de1003eada34eee6c63eb2a9402a29
leejss <goldemshine@gmail.com>
2026-08-03 00:46:56 +0900

    style: order site header imports

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import { PanelLeftOpenIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { PanelLeftOpenIcon } from "lucide-react";
 
 import chaekIcon from "@/app/icon.png";
 import { AccountMenu } from "@/components/account-menu";
